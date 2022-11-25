401bf3d0cf93cd572db239284848f6e80c14f24b
Kaloyan Kondov <kaloyankondow@gmail.com>
2022-11-26 01:28:01 +0200

    Custom Error Handling

--- src/main/java/com/example/ownwebsite/config/SecurityConfig.java
+++ src/main/java/com/example/ownwebsite/config/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig {
         return http.authorizeRequests()
                 .antMatchers("/js/**","/css/**","/img/**").permitAll()
                 .antMatchers("/","/users/login","/users/register").permitAll()
-                .antMatchers("/**").authenticated()
+                .antMatchers("/articles/add").access("hasRole('ADMIN')")
                 .and()
                 .formLogin().loginPage("/users/login")
                 .usernameParameter(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY)
